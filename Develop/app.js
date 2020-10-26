05e27612873ec735599cb5067cf8069cbbc66982
Mongolmaa Gurbazar <mongolmaa555@gmail.com>
2020-10-26 15:47:22 -0400

    It's adding new note :) not overwriting :)

--- Develop/app.js
+++ Develop/app.js
@@ -41,30 +41,60 @@ app.post("/api/notes", function (req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
   let newNote = req.body;
-  let arrNote = [];
-  arrNote.concat(
-    fs.readFile("./db/db.json", "utf8", function (err) {
-      if (err) {
-        throw err;
-      }
-    })
-  );
-
-  arrNote.push(newNote);
-  console.log("arr", arrNote);
-  console.log(newNote);
-  fs.writeFile(
-    path.join(__dirname, "db/db.json"),
-    JSON.stringify(arrNote),
-    function (err) {
-      if (err) {
-        throw err;
-      }
+  fs.readFile("./db/db.json", "utf8", function (err, data) {
+    if (err) {
+      throw err;
     }
-  );
+    const jsonData = JSON.parse(data);
+    newNote.id = jsonData.length + 1;
+    jsonData.push(newNote);
+
+    fs.writeFile(
+      path.join(__dirname, "db/db.json"),
+      JSON.stringify(jsonData),
+      function (err) {
+        if (err) {
+          throw err;
+        }
+        res.json(data[data.length]);
+        res.json(jsonData);
+      }
+    );
+  });
 });
 
 //DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+app.delete("/api/notes/:id", function (err, data) {
+  fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", function (
+    err,
+    data
+  ) {
+    if (err) {
+      throw err;
+    }
+    const id = parseInt(req.params.id);
+    let db = JSON.parse(data);
+    for (let i = 0; i < db.length; i++) {
+      if (db[i].is === id) {
+        db.splice(i, 1);
+        for (let i = 0; i < db.length; i++) {
+          db[i].id = i + 1;
+        }
+        return fs.writeFile(
+          path.join(__dirname, "/db/db.json"),
+          JSON.stringify(db),
+          function (err) {
+            if (err) {
+              throw err;
+            }
+            res.end();
+          }
+        );
+      }
+    }
+    res.end();
+  });
+});
 
 //Starts the server to begin listening
 app.listen(PORT, function () {
