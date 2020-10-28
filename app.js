23e3c3dc7a912073506ad34b6ad06e4414dc6bff
Mongolmaa Gurbazar <mongolmaa555@gmail.com>
2020-10-28 12:37:48 -0400

    after removal of develop folder needed to get change path of ./public/assets to public/assets in app.js, line 13

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const PORT = 3003;
 //Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("./public/assets"));
+app.use(express.static("public/assets"));
 const dir = __dirname + "/public";
 
 //Basic route that sends the user first to the AJAX page
